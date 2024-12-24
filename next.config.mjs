17a7e9a3abda03b4ecccbc0e3142e8a06e0a470d
Danish <iamdan10ish@gmail.com>
2024-12-24 16:42:42 +0300

    Fixed GH Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", 
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/<your-repo-name>" : undefined,
+  images: {
+    unoptimized: true, 
+  },
+};
 
 export default nextConfig;
